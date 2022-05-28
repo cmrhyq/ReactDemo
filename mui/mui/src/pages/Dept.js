fead9b6758a28866eae6dbbbce4e89e8a2856c3c
i拥玥 <1678336204@qq.com>
2022-05-28 16:20:52 +0800

    create demo

---
+++ mui/mui/src/pages/Dept.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Dept extends React.Component {
+    render() {
+        return (
+            <div>
+                Dept
+            </div>
+        )
+    }
+}
+
+export default Dept;
